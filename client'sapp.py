9558bc4639f4b4d7fd0a4cc40c9b3da6c55b321a
Farren-C <69100667+Farren-C@users.noreply.github.com>
2021-09-27 16:34:13 +0200

    Update client'sapp.py

--- client'sapp.py
+++ client'sapp.py
@@ -64,32 +64,17 @@ st.write('number of classes:', len(np.unique(y)))
 
 def add_parameter_ui(clf_name):
     params = dict()
-    if clf_name == 'SVM':
-        C = st.sidebar.slider('C', 0.01, 10.0)
-        params['C'] = C
-    elif clf_name == 'GB':
-        max_depth = st.sidebar.slider('max_depth', 2, 15)
-        params['max_depth'] = max_depth
-        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
-        params['n_estimators'] = n_estimators
-    else:
-        max_depth = st.sidebar.slider('max_depth', 2, 15)
-        params['max_depth'] = max_depth
-        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
-        params['n_estimators'] = n_estimators
+    clf_name = 'Random Forest'
+    max_depth = st.sidebar.slider('max_depth', 2, 15)
+    params['max_depth'] = max_depth
+    n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+    params['n_estimators'] = n_estimators
     return params
 
 params = add_parameter_ui(classifier_name)
 
-def get_classifier(clf_name, params):
-    clf = None
-    if clf_name == 'SVM':
-        clf = SVC(C=params['C'])
-    elif clf_name == 'GB':
-        clf = GradientBoostingClassifier(n_estimators=params['n_estimators'],max_depth=params['max_depth'],random_state=1234)
-    else:
-        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
-            max_depth=params['max_depth'], random_state=1234)
+def get_classifier(clf, params):
+    clf = RandomForestClassifier(n_estimators=params['n_estimators'])
     return clf
 
 clf = get_classifier(classifier_name, params)
